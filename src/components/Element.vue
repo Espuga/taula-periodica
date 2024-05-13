8b361384a9130140fd429bf52ab506dd69af3a47
Espuga <marcespugamoreno@gmail.com>
2024-05-13 22:00:56 +0200

    first commit

---
+++ src/components/Element.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="computedClasses">
+    <div class="absolute top-0 left-1">
+      {{ props.atomicNumber }}
+    </div>
+    {{ props.simbol }}
+    <div class="absolute bottom-0 w-full flex justify-center">
+      {{ props.name }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    visible: Boolean,
+    classe: String,
+    simbol: String,
+    atomicNumber: String,
+    name: String
+  });
+
+  const computedClasses = `col-span-${props.classe} h-20 w-20 relative flex items-center justify-center ${props.visible ? 'border border-slate-950' : ''}`;
+</script>
+
+<style scoped>
+</style>
